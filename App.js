244e564e4697dced92d295ce89f3e1f24680fa9c
P . Sai Charan Kumar <87646166+charanccherry@users.noreply.github.com>
2021-08-28 20:23:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,3 +5,18 @@ import './App.css'
 const App = () => <ClickCounter />
 
 export default App
+
+#App.css
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
